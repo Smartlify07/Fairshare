aa99fec31fc1e7d11cb36b0e21620c357f1a98ae
Obinna Anosike <smartlify09@gmail.com>
2025-11-09 08:38:53 +0100

    debug url

--- src/js/scripts/signup.ts
+++ src/js/scripts/signup.ts
@@ -1,28 +1,29 @@
-import { supabase } from "../../supabase";
+import { supabase } from '../../supabase';
 
 const signupButton = document.querySelector(
-  "#auth-signup-google"
+  '#auth-signup-google'
 ) as HTMLButtonElement;
 
 const getURL = () => {
   let url =
-    import.meta?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    import.meta?.env?.VITE_SITE_URL ?? // Set this to your site URL in production env.
     import.meta?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:5173/";
+    'http://localhost:5173/';
   // Make sure to include `https://` when not localhost.
-  url = url.startsWith("http") ? url : `https://${url}`;
+  url = url.startsWith('http') ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
-  url = url.endsWith("/") ? url : `${url}/`;
+  url = url.endsWith('/') ? url : `${url}/`;
   return url;
 };
 const handleSignUpWithGoogle = async () => {
   await supabase.auth.signInWithOAuth({
-    provider: "google",
+    provider: 'google',
     options: {
       redirectTo: getURL(),
     },
   });
 };
-signupButton.addEventListener("click", () => {
+console.log('URL', getURL());
+signupButton.addEventListener('click', () => {
   handleSignUpWithGoogle();
 });
